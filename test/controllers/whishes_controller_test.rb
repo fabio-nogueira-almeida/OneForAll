3cbd006c5535a15b248d23736ace9c8fd09e4a0e
Fábio Nogueira de Almeida <fna.contact@gmail.com>
2020-09-30 22:54:54 -0300

    remove whishes tests

--- test/controllers/whishes_controller_test.rb
+++ test/controllers/whishes_controller_test.rb
@@ -1,38 +1,38 @@
 require 'test_helper'
 
 class WhishesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @whish = whishes(:one)
-  end
+  # setup do
+  #   @whish = whishes(:one)
+  # end
 
-  test "should get index" do
-    get whishes_url, as: :json
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get whishes_url, as: :json
+  #   assert_response :success
+  # end
 
-  test "should create whish" do
-    assert_difference('Whish.count') do
-      post whishes_url, params: { whish: { goalAmount: @whish.goalAmount, goalDate: @whish.goalDate, name: @whish.name, totalBalance: @whish.totalBalance } }, as: :json
-    end
+  # test "should create whish" do
+  #   assert_difference('Whish.count') do
+  #     post whishes_url, params: { whish: { goalAmount: @whish.goalAmount, goalDate: @whish.goalDate, name: @whish.name, totalBalance: @whish.totalBalance } }, as: :json
+  #   end
 
-    assert_response 201
-  end
+  #   assert_response 201
+  # end
 
-  test "should show whish" do
-    get whish_url(@whish), as: :json
-    assert_response :success
-  end
+  # test "should show whish" do
+  #   get whish_url(@whish), as: :json
+  #   assert_response :success
+  # end
 
-  test "should update whish" do
-    patch whish_url(@whish), params: { whish: { goalAmount: @whish.goalAmount, goalDate: @whish.goalDate, name: @whish.name, totalBalance: @whish.totalBalance } }, as: :json
-    assert_response 200
-  end
+  # test "should update whish" do
+  #   patch whish_url(@whish), params: { whish: { goalAmount: @whish.goalAmount, goalDate: @whish.goalDate, name: @whish.name, totalBalance: @whish.totalBalance } }, as: :json
+  #   assert_response 200
+  # end
 
-  test "should destroy whish" do
-    assert_difference('Whish.count', -1) do
-      delete whish_url(@whish), as: :json
-    end
+  # test "should destroy whish" do
+  #   assert_difference('Whish.count', -1) do
+  #     delete whish_url(@whish), as: :json
+  #   end
 
-    assert_response 204
-  end
+  #   assert_response 204
+  # end
 end
